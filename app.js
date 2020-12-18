4e09ce401f95af8b2d64d0e86d99920dc93b7980
Hebin Aloor <hebin.aloor@infovision.com>
2020-12-18 21:51:35 +0530

    Added tap and swipe gesture for testing

--- app.js
+++ app.js
@@ -110,6 +110,7 @@ class App {
   }
 
   createUI() {
+    console.log("Entering here");
     const config = {
       panelSize: { width: 0.2, height: 0.05 },
       height: 128,
@@ -142,6 +143,28 @@ class App {
     const btn = new ARButton(this.renderer, { onSessionStart, onSessionEnd });
 
     //Add gestures here
+    this.gestures = new ControllerGestures(this.renderer);
+
+    this.gestures.addEventListener("tap", (ev) => {
+      console.log("tap");
+      self.ui.updateElement("info", "tap");
+
+      if (!self.knight.object.visible) {
+        self.knight.object.visible = true;
+        self.knight.object.position.set(0, -0.3, -0.5).add(ev.position);
+        self.scene.add(self.knight.object);
+      }
+    });
+
+    this.gestures.addEventListener("swipe", (ev) => {
+      console.log(ev);
+      self.ui.updateElement("info", `swipe ${ev.direction}`);
+
+      if (self.knight.object.visible) {
+        self.knight.object.visible = false;
+        self.scene.remove(self.knight.object);
+      }
+    });
 
     this.renderer.setAnimationLoop(this.render.bind(this));
   }
